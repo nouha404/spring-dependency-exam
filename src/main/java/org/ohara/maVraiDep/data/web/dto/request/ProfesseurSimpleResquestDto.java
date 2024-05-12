de7cc37ce61cb891c9ab63097b7cef4c5c30ec90
nouha404 <aboubacar.marega@ism.edu.sn>
2024-05-12 18:42:53 +0200

    add prof request dto as dependency

---
+++ src/main/java/org/ohara/maVraiDep/data/web/dto/request/ProfesseurSimpleResquestDto.java
@@ -0,0 +1,33 @@
+package org.ohara.maVraiDep.data.web.dto.request;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.ohara.maVraiDep.data.entitties.Professeur;
+import org.ohara.maVraiDep.data.entitties.Semestre;
+import org.ohara.maVraiDep.data.enums.ENiveau;
+import org.ohara.maVraiDep.data.enums.Specialiter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProfesseurSimpleResquestDto {
+    private String nom;
+    private String prenom;
+    @Enumerated(value = EnumType.STRING)
+    private Specialiter specialite;
+    private ENiveau grade;
+
+    public Professeur TransformToEntity(){
+        return Professeur.builder()
+                .nom(nom)
+                .prenom(prenom)
+                .specialite(specialite)
+                .grade(grade)
+                .build();
+    }
+}
